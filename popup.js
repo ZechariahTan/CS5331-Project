b5077ec9d53c4d35c47625c978965a7fd00d054a
Zechariah Tan <zx3110@gmail.com>
2022-04-07 19:07:04 +0800

    Add button to replace input value
    Add functionality for attack to work on https://eyeondesign.aiga.org/

--- popup.js
+++ popup.js
@@ -14,32 +14,76 @@ changeColor.addEventListener("click", async () => {
     });
 });
 
-  
-// The body of this function will be executed as a content script inside the
-// current page
 function setPageBackgroundColor() {
-    let ele = document.querySelectorAll('input[name*=query]');
+    let ele = document.querySelectorAll('input[name*=s]');
     // console.log(ele);
     var copiedEle;
     var copiedDiv;
     var parentNode;
+    // var rect;
+    var oTop;
+    var oLeft;
     ele.forEach(element => {
         copiedEle = element;
         copiedDiv = element.parentNode;
-        parentNode = copiedDiv.parentNode;
-        element.hidden=true;
+        oTop = element.offsetTop;
+        oLeft = element.offsetLeft;
+        element.style = "visibility: hidden;"
     });
-    console.log(parentNode)
-    const div = document.createElement('div');
-    div.class = 'replacement';
-    div.innerHTML = '<input data-testid="search-input" class="css-1axrnfw" type="text" name="query" placeholder="SEARCH" value="testing">';
-    console.log(div.innerHTML);
-    parentNode.appendChild(div);
+
+    let newhtml = copiedEle.outerHTML;
+    newhtml = newhtml.replace(/name=".+"/,"name=\"dummySearchField\"");
+
+    var div = document.createElement('input');
+    div.class = copiedEle.class;
+    div.name = "dummySearchField";
+    div.type = "search";
+    div.style.position="absolute";
+    div.style.left = oLeft;
+    div.style.top = oTop;
+    console.log(div)
+    copiedDiv.appendChild(div);
+
+    console.log("replaced");
 
 
 chrome.storage.sync.get("color", ({ color }) => {
     document.body.style.backgroundColor = color;
 });
+}
+
+searchEntered.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: fillHiddenSearchBar,
+    });
+});
+  
+// The body of this function will be executed as a content script inside the
+// current page
+function fillHiddenSearchBar() {
+    let ele = document.querySelectorAll('input[name=s]');
+
+    var copiedEle;
+    var copiedDiv;
+    var parentNode;
+
+    var oTop;
+    var oLeft;
+    ele.forEach(element => {
+        copiedEle = element;
+        copiedDiv = element.parentNode;
+        parentNode = copiedDiv.parentNode;
+        element.value = "worlds first emoji"
+        element.hidden = true;
+    });
+    console.log("filled")
+
+    chrome.storage.sync.get("color", ({ color }) => {
+        document.body.style.backgroundColor = '#545432';
+    });
 }
 
 function test() {
